2977a6e6c1c0273cc24012dd667dd6073a40bfc1
Alex Garrett-Smith <alex@codecourse.com>
2021-10-07 18:03:03 +0100

    Episode 6

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  Home
+  <form v-on:submit.prevent="login">
+      <div>
+          <label for="email">Email</label>
+          <input type="text" name="email" id="email" v-model="form.email">
+      </div>
+
+      <div>
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" v-model="form.password">
+      </div>
+
+      <div>
+          <button type="submit">Login</button>
+      </div>
+  </form>
 </template>
 
 <script setup>
+    import axios from 'axios'
+    import { reactive } from 'vue'
 
+    const form = reactive({
+        email: '',
+        password: ''
+    })
+
+    const login = async () => {
+        await axios.get('/sanctum/csrf-cookie')
+
+        await axios.post('/login', form)
+    }
 </script>
